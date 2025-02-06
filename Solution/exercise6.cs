ab00281e23dc6f600f6cbe4f6dd59b1d5fa07c61
Thomas Curti <t.curti@groupeonepoint.com>
2025-02-06 10:42:04 +0100

    Make it iso

---
+++ Solution/exercise6.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercice6;
+
+public class Person
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
+
+public class Student : Person
+{
+    public string Course { get; }
+
+    public Student(string firstName, string lastName, string course)
+        : base(firstName, lastName)
+    {
+        Course = course;
+    }
+
+    public string GetDetails()
+    {
+        return $"{GetFullName()} is enrolled in {Course}";
+    }
+}
