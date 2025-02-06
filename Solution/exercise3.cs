9b9388c3f8a78dbeb4dc13f806206e57e55b4d9c
Thomas Curti <t.curti@groupeonepoint.com>
2025-02-06 10:32:39 +0100

    Make it iso

---
+++ Solution/exercise3.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercice3;
+
+public class Exercice3
+{
+    public static List<List<string>> SizeSplitter(List<string> words, int size)
+    {
+        var group1 = new List<string>();
+        var group2 = new List<string>();
+
+        foreach (var word in words)
+        {
+            if (word.Length == size)
+            {
+                group1.Add(word);
+            }
+            else
+            {
+                group2.Add(word);
+            }
+        }
+
+        group1.Sort();
+        group2.Sort();
+
+        return new List<List<string>> { group1, group2 };
+    }
+}
+
